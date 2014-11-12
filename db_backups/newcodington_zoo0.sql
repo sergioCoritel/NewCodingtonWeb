7603c884e14b16ba4ba2b1536a1a37c610d0d9da
marloonsal <marloon.so@gmail.com>
2014-11-12 06:44:10 -0600

    actualizacion bd

---
+++ db_backups/newcodington_zoo0.sql
@@ -0,0 +1,61 @@
+CREATE DATABASE  IF NOT EXISTS `newcodington` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `newcodington`;
+-- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
+--
+-- Host: localhost    Database: newcodington
+-- ------------------------------------------------------
+-- Server version	5.5.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `zoo`
+--
+
+DROP TABLE IF EXISTS `zoo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `zoo` (
+  `placeId` int(11) NOT NULL,
+  `placetypeid` int(11) NOT NULL DEFAULT '8',
+  `name` varchar(45) DEFAULT NULL,
+  `capacity` int(11) DEFAULT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `hour_open` time DEFAULT NULL,
+  `hour_close` time DEFAULT NULL,
+  `zone` varchar(45) DEFAULT NULL,
+  `image` longblob,
+  `building` tinyint(4) DEFAULT '0',
+  PRIMARY KEY (`placeId`,`placetypeid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `zoo`
+--
+
+LOCK TABLES `zoo` WRITE;
+/*!40000 ALTER TABLE `zoo` DISABLE KEYS */;
+INSERT INTO `zoo` VALUES (1,8,'zoo madrid',200,'zoo divertido','00:02:00',NULL,'north','zip',0);
+/*!40000 ALTER TABLE `zoo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-11-12  6:41:07
