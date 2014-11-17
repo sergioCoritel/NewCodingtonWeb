5638c26e198f15ffd166e9bd6d9c9c6d421e987d
juanchusoria <juanchusoria@hotmail.com>
2014-11-17 04:12:16 -0600

    DAOS

--- NewCodingtonJavaCore/src/com/newcodingtoncity/model/services/EventService.java
+++ NewCodingtonJavaCore/src/com/newcodingtoncity/model/services/EventService.java
@@ -1,5 +1,29 @@
 package com.newcodingtoncity.model.services;
 
-public class EventService {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.newcodingtoncity.model.daos.DAOManager;
+import com.newcodingtoncity.model.daos.EventDAO;
+import com.newcodingtoncity.model.domain.*;
+import com.newcodingtoncity.model.interfaces.daos.IEventDAO;
+
+
+
+public class EventService {
+	
+	public List<Event> getAllEvents(){
+		
+		DAOManager manager = null;
+		IEventDAO eventDAO = null;
+		List<Event> allEvents = new ArrayList<Event>();
+		
+		manager = new DAOManager();
+		eventDAO = manager.getEventDAO();
+
+		
+		return allEvents;
+		
+	}
+	
 }
