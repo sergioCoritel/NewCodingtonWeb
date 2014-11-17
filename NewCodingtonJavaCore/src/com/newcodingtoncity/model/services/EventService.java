1894f5630506d3ea5f4dabf148deae25a7458d5d
juanchusoria <juanchusoria@hotmail.com>
2014-11-17 05:42:01 -0600

    EventService

--- NewCodingtonJavaCore/src/com/newcodingtoncity/model/services/EventService.java
+++ NewCodingtonJavaCore/src/com/newcodingtoncity/model/services/EventService.java
@@ -1,11 +1,12 @@
 package com.newcodingtoncity.model.services;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.newcodingtoncity.model.daos.DAOManager;
-import com.newcodingtoncity.model.daos.EventDAO;
-import com.newcodingtoncity.model.domain.*;
+import com.newcodingtoncity.model.domain.Event;
 import com.newcodingtoncity.model.interfaces.daos.IEventDAO;
 
 
@@ -18,9 +19,24 @@ public class EventService {
 		IEventDAO eventDAO = null;
 		List<Event> allEvents = new ArrayList<Event>();
 		
-		manager = new DAOManager();
-		eventDAO = manager.getEventDAO();
-
+		
+		
+		try {
+			manager = new DAOManager();
+			eventDAO = manager.getEventDAO();
+			
+			allEvents = eventDAO.showAllEvents();
+			manager.closeConnection();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return allEvents;
 		
