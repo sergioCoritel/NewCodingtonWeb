3aa63c775db1cb88a3697fc92d674d05efaa6274
JuanMaGP <juanmagp10@gmail.com>
2014-11-17 14:45:10 +0100

    User and validators

---
+++ NewCodingtonJavaCore/src/com/newcodingtoncity/model/exceptions/DomainException.java
@@ -0,0 +1,21 @@
+package com.newcodingtoncity.model.exceptions;
+
+/**
+ * Representa las excepciones lanzadas en paquete domain
+ * 
+ */
+
+public class DomainException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param message
+	 */
+	public DomainException(String message){
+		super(message);
+	}
+	
+}
